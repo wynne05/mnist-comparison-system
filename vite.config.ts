e4d061e06ae65ac44a2c98167b933e8eff2c1e95
wynne05 <jinghanxu53@gmail.com>
2026-06-08 13:14:54 +0800

    修改viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,29 @@
-import path from "path";
-import { fileURLToPath } from "url";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
+// import path from "path";
+// import { fileURLToPath } from "url";
+// import tailwindcss from "@tailwindcss/vite";
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import { viteSingleFile } from "vite-plugin-singlefile";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss(), viteSingleFile()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "src"),
+//     },
+//   },
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), viteSingleFile()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
+  plugins: [react()],
+  base: '/mnist-comparison-system/', // <--- 注意：这里填您在 GitHub 上的仓库名称，前后必须都有斜杠，例如 '/mnistcnn/'
+})
